1ba4b7f856930d29fdc015b5426d8505869c4fbe
Imthiyas V <imthiyasv@pop-os.localdomain>
2020-07-28 19:56:48 +0530

    Initial commit

---
+++ add2numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int n1,n2;
+  cout<<"Enter the first number : " ;
+  cin>>n1;
+  cout<<"Enter the second number : " ;
+  cin>>n2;
+  int sum = n1+n2;
+  cout<<"Sum of given two numbers is "<<sum;
+
+  return 0;
+
+}
